53ad1e0eae7d4e3cbe67d34cbc6d7fffb8e02c6c
Drew <tribalxghost@gmail.com>
2024-03-16 21:32:09 -0500

    update

--- app/server/database.js
+++ app/server/database.js
@@ -7,16 +7,16 @@ let db;
 
 if (process.env.NODE_ENV === "production") {
   db = new Client({
-    host: "localhost",
-    user: "postgres",
+    host: "dpg-cnp6960l5elc73cl7td0-a",
+    user: "budgetdb_bkqh_user",
     port: 5432,
     password: process.env.PASS,
     database: process.env.DATABASE_URL
   });
 } else {
   db = new Client({
-    host: "localhost",
-    user: "postgres",
+    host: "dpg-cnp6960l5elc73cl7td0-a",
+    user: "budgetdb_bkqh_user",
     port: 5432,
     password: process.env.PASS || "Christopher#0160",
     database: process.env.DATABASE_URL 
