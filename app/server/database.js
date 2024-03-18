d9b6e9eda92622dc3c5df885c7c6d5e6ea8b1125
Drew <tribalxghost@gmail.com>
2024-03-18 18:01:01 -0500

    update

--- app/server/database.js
+++ app/server/database.js
@@ -5,23 +5,26 @@ const { getDatabaseUri } = require("./config");
 
 let db;
 
-if (process.env.NODE_ENV === "production") {
-  db = new Client({
-    host: "dpg-cnp6960l5elc73cl7td0-a",
-    user: "budgetdb_bkqh_user",
-    port: 5432,
-    password: process.env.PASS,
-    database: process.env.DB
-  });
-} else {
-  db = new Client({
-    host: "dpg-cnp6960l5elc73cl7td0-a",
-    user: "budgetdb_bkqh_user",
-    port: 5432,
-    password: process.env.PASS || "Christopher#0160",
-    database: process.env.DB 
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   db = new Client({
+//     host: "dpg-cnp6960l5elc73cl7td0-a",
+//     user: "budgetdb_bkqh_user",
+//     port: 5432,
+//     password: process.env.PASS,
+//     database: process.env.DB
+//   });
+// } else {
+//   db = new Client({
+//     host: "dpg-cnp6960l5elc73cl7td0-a",
+//     user: "budgetdb_bkqh_user",
+//     port: 5432,
+//     password: process.env.PASS || "Christopher#0160",
+//     database: process.env.DB 
+//   });
+// }
+
+
+db = new Client(process.env.DATABASE_URL)
 try{
   db.connect();
 
